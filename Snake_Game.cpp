76c07a4c4bcd51469948a58b5490e5ed11642a22
Phu Le <25730133@ms.uit.edu.vn>
2026-04-17 20:21:56 +0700

    Take Bait
    develop take bait feature

--- Snake_Game.cpp
+++ Snake_Game.cpp
@@ -2,34 +2,56 @@
 #include <windows.h>
 #include <cstdlib>
 #include <conio.h>
+#include <time.h>
+#define MINX 2
+#define MINY 2
+#define MAXX 35
+#define MAXY 20
 using namespace std;
 void gotoxy( int column, int line );
-struct Point{
+struct Point
+{
     int x,y;
 };
-class CONRAN{
+class CONRAN
+{
 public:
     struct Point A[100];
     int DoDai;
-    CONRAN(){
+    CONRAN()
+    {
         DoDai = 3;
-        A[0].x = 10; A[0].y = 10;
-        A[1].x = 11; A[1].y = 10;
-        A[2].x = 12; A[2].y = 10;
+        A[0].x = 10;
+        A[0].y = 10;
+        A[1].x = 11;
+        A[1].y = 10;
+        A[2].x = 12;
+        A[2].y = 10;
     }
-    void Ve(){
-        for (int i = 0; i < DoDai; i++){
+    void Ve(Point Qua)
+    {
+        for (int i = 0; i < DoDai; i++)
+        {
             gotoxy(A[i].x,A[i].y);
             cout<<"X";
         }
+        gotoxy(Qua.x, Qua.y);
+        cout<<"*";
     }
-    void DiChuyen(int Huong){
-        for (int i = DoDai-1; i>0;i--)
+    void DiChuyen(int Huong, Point& Qua)
+    {
+        for (int i = DoDai-1; i>0; i--)
             A[i] = A[i-1];
         if (Huong==0) A[0].x = A[0].x + 1;
         if (Huong==1) A[0].y = A[0].y + 1;
         if (Huong==2) A[0].x = A[0].x - 1;
         if (Huong==3) A[0].y = A[0].y - 1;
+        if ((A[0].x == Qua.x) && (A[0].y == Qua.y))
+        {
+            DoDai++;
+            Qua.x = rand()%(MAXX-MINX)+MINX;
+            Qua.y = rand()%(MAXY-MINY)+MINY;
+        }
 
     }
 };
@@ -39,9 +61,15 @@ int main()
     CONRAN r;
     int Huong = 0;
     char t;
+    Point Qua;
+    srand((int)time(0));
+    Qua.x = rand()%(MAXX-MINX)+MINX;
+    Qua.y = rand()%(MAXY-MINY)+MINY;
 
-    while (1){
-        if (kbhit()){
+    while (1)
+    {
+        if (kbhit())
+        {
             t = getch();
             if (t=='a') Huong = 2;
             if (t=='w') Huong = 3;
@@ -49,8 +77,8 @@ int main()
             if (t=='x') Huong = 1;
         }
         system("cls");
-        r.Ve();
-        r.DiChuyen(Huong);
+        r.Ve(Qua);
+        r.DiChuyen(Huong, Qua);
         Sleep(300);
     }
 
@@ -59,12 +87,12 @@ int main()
 
 
 void gotoxy( int column, int line )
-  {
-  COORD coord;
-  coord.X = column;
-  coord.Y = line;
-  SetConsoleCursorPosition(
-    GetStdHandle( STD_OUTPUT_HANDLE ),
-    coord
+{
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    SetConsoleCursorPosition(
+        GetStdHandle( STD_OUTPUT_HANDLE ),
+        coord
     );
-  }
+}
