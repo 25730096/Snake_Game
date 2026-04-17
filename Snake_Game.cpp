233c7a81c5ed1d9985eedeb242903f8c39eba1de
25730152 <25730152@ms.uit.edu.vn>
2026-04-17 20:27:54 +0700

    Draw-Frame
    Draw Frame feather

--- Snake_Game.cpp
+++ Snake_Game.cpp
@@ -2,29 +2,43 @@
 #include <windows.h>
 #include <cstdlib>
 #include <conio.h>
+#include <time.h>
+#define MINX 2
+#define MINY 2
+#define MAXX 35
+#define MAXY 20
 using namespace std;
 void gotoxy( int column, int line );
-struct Point{
+struct Point
+{
     int x,y;
 };
-class CONRAN{
+class CONRAN
+{
 public:
     struct Point A[100];
     int DoDai;
-    CONRAN(){
+    CONRAN()
+    {
         DoDai = 3;
-        A[0].x = 10; A[0].y = 10;
-        A[1].x = 11; A[1].y = 10;
-        A[2].x = 12; A[2].y = 10;
+        A[0].x = 10;
+        A[0].y = 10;
+        A[1].x = 11;
+        A[1].y = 10;
+        A[2].x = 12;
+        A[2].y = 10;
     }
-    void Ve(){
-        for (int i = 0; i < DoDai; i++){
+    void Ve()
+    {
+        for (int i = 0; i < DoDai; i++)
+        {
             gotoxy(A[i].x,A[i].y);
             cout<<"X";
         }
     }
-    void DiChuyen(int Huong){
-        for (int i = DoDai-1; i>0;i--)
+    void DiChuyen(int Huong)
+    {
+        for (int i = DoDai-1; i>0; i--)
             A[i] = A[i-1];
         if (Huong==0) A[0].x = A[0].x + 1;
         if (Huong==1) A[0].y = A[0].y + 1;
@@ -33,6 +47,16 @@ public:
 
     }
 };
+void VeKhung()
+{
+    for (int i = MINX ; i<=MAXX ; i++)
+        for (int j = MINX ; j<=MAXY ; j++)
+            if ((i==MINX) || (i==MAXX) || (j==MINY) || (j==MAXY))
+            {
+                gotoxy(i,j);
+                printf("+");
+            }
+}
 
 int main()
 {
@@ -40,8 +64,10 @@ int main()
     int Huong = 0;
     char t;
 
-    while (1){
-        if (kbhit()){
+    while (1)
+    {
+        if (kbhit())
+        {
             t = getch();
             if (t=='a') Huong = 2;
             if (t=='w') Huong = 3;
@@ -49,6 +75,7 @@ int main()
             if (t=='x') Huong = 1;
         }
         system("cls");
+        VeKhung();
         r.Ve();
         r.DiChuyen(Huong);
         Sleep(300);
@@ -59,12 +86,12 @@ int main()
 
 
 void gotoxy( int column, int line )
-  {
-  COORD coord;
-  coord.X = column;
-  coord.Y = line;
-  SetConsoleCursorPosition(
-    GetStdHandle( STD_OUTPUT_HANDLE ),
-    coord
+{
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    SetConsoleCursorPosition(
+        GetStdHandle( STD_OUTPUT_HANDLE ),
+        coord
     );
-  }
+}
